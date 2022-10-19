c829410e3101f5a6db58a6b5ea4524990b9e3d53
MOBILE DEVELOPMENT <mobiledevelopmentafternoon@DSIs-iMac.lan>
2022-10-19 11:50:11 +0100

    my first trial

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:food/lundary_data.dart';
 
 void main() {
   runApp(const MyApp());
@@ -14,23 +13,118 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         appBar: AppBar(
           centerTitle: true,
-          // leading: const Icon(Icons.menu),
           title: const Text("iLundary"),
-          actions: const [Icon(Icons.add)],
         ),
         drawer: const Drawer(),
-        body: Center(
+        body: Column(children: const [
+          SizedBox(height: 20.5),
+          Center(
+            child: Text(
+              "My Lundary Data",
+              style: TextStyle(
+                  fontSize: 25.0,
+                  fontWeight: FontWeight.w800,
+                  color: Colors.blue),
+            ),
+          ),
+          MyOrder(),
+          StatData(),
+          CustomStack(),
+        ]),
+      ),
+    );
+  }
+}
+
+class MyOrder extends StatelessWidget {
+  const MyOrder({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.only(top: 30, left: 25, right: 25),
+      height: 150.0,
+      width: double.infinity,
+      color: Colors.blue,
+      child: Center(
+          child: Text(
+        "My Orders".toUpperCase(),
+        style: const TextStyle(color: Colors.white, fontSize: 25),
+      )),
+    );
+  }
+}
+
+class StatData extends StatelessWidget {
+  const StatData({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        const SizedBox(
+          width: 25.0,
+        ),
+        Container(
+          height: 100,
+          width: 150,
+          color: Colors.blue,
+          margin: const EdgeInsets.only(top: 30),
+          child: const Center(
+              child: Text(
+            '42\nCompleted',
+            textAlign: TextAlign.center,
+          )),
+        ),
+        Container(
+          margin: const EdgeInsets.only(top: 30, left: 40.0),
+          padding: const EdgeInsets.only(left: 15.0),
+          height: 100,
+          width: 150,
+          color: Colors.blue,
+          child: const Center(
+              child: Text(
+            '20\nPending',
+            textAlign: TextAlign.center,
+            style: TextStyle(color: Colors.white),
+          )),
+        )
+      ],
+    );
+  }
+}
+
+class CustomStack extends StatelessWidget {
+  const CustomStack({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        Container(
+          margin: const EdgeInsets.only(left: 30.0, right: 30.0, top: 30),
+          padding: const EdgeInsets.all(30),
+          width: double.infinity,
+          color: Colors.lightBlueAccent,
           child: Column(
-            children: const [
-              Text(
-                "Counter",
-                style: TextStyle(fontSize: 39),
+            children: [
+              Container(
+                width: double.infinity,
+                color: Colors.orange,
+                padding: const EdgeInsets.all(20),
+                child: const Center(child: Text("Place Order")),
               ),
-              AddCount(),
+              const SizedBox(height: 20.0),
+              Container(
+                width: double.infinity,
+                color: Colors.pink,
+                padding: const EdgeInsets.all(20),
+                child: const Center(child: Text("Exit")),
+              ),
             ],
           ),
-        ),
-      ),
+        )
+      ],
     );
   }
 }
